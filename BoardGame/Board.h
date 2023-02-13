872edaea8148561c97ba778454955aa513f98162
shirs12 <shirseg98@gmail.com>
2023-02-13 19:24:57 +0200

    update

--- BoardGame/Board.h
+++ BoardGame/Board.h
@@ -1,5 +1,33 @@
+#include <iostream>
+using namespace std;
+
 #pragma once
 class Board
 {
+private:
+
+	// TODO: change matrix to 31x31 dimensions.
+	char matrix[11][11] = {};
+
+	const int SIZE = sizeof(matrix[0]);
+
+public:
+
+	// constructor
+	Board();
+
+	// methods:
+
+	// initialize player's point on board
+	void initPlayer(char num, int row, int col);
+
+	// initialize winning-mat on board
+	void initWinningMat(int row, int col, int height, int width);
+
+	//checks if one of the players won
+	bool isOnWinningMat(int row, int col);
+
+	// print matrix
+	void printBoard();
 };
 
--- BoardGame/Player.h
+++ BoardGame/Player.h
@@ -1,5 +1,28 @@
+#include <iostream>
+using namespace std;
+
 #pragma once
 class Player
 {
+public:
+	char cNumber = '0';
+	int nStartPointRow = 0;
+	int nStartPointCol = 0;
+	int nCurrentPointRow = 0;
+	int nCurrentPointCol = 0;
+
+	// constructor
+	Player(char num, int row, int col);
+
+	// methods
+	void moveUp();
+
+	void moveDown();
+
+	void moveLeft();
+
+	void moveRight();
+
+
 };
 
