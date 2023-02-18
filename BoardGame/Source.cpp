37ad7e498cd851a807dcfc01eba1a5f2adbac8af
shirs12 <shirseg98@gmail.com>
2023-02-18 21:12:01 +0200

    update

--- BoardGame/Source.cpp
+++ BoardGame/Source.cpp
@@ -47,7 +47,7 @@ void switchTurns(bool flag1, bool flag2) {
 int main() {
 
 #pragma region Variables Declaration
-	int choice;
+	int choice = 0;
 
 	Board board;
 
@@ -73,118 +73,117 @@ int main() {
 #pragma endregion
 
 #pragma region User Input - Initial Board & Players
-
-	cout << "Welcome!" << endl;
-	cout << "Menu:"
-		"\n1. start the game"
-		"\n2. Exit"
-		"\nEnter your choice: ";
-	cin >> choice;
-	cout << endl;
-
-	if (choice > 2 || choice < 1)
+	while (choice != 2)
 	{
-		cout << "Wrong choice." << endl;
-	}
-	else if (choice == 1)
-	{
-		cout << "You chose to play!\n" << endl;
-
-		cout << "The game board is empty, you need to fill it up" << endl;
-		board.printBoard();
-
-		// input row and col from user - first player
-		cout << "First player starting point\nenter row: ";
-		cin >> firstPlayerRow;
-		cout << "\nenter colomn: ";
-		cin >> firstPlayerCol;
+		cout << "Welcome!" << endl;
+		cout << "Menu:"
+			"\n1. start the game"
+			"\n2. Exit"
+			"\nEnter your choice: ";
+		cin >> choice;
 		cout << endl;
 
-		Player firstPlayer('1', firstPlayerRow, firstPlayerCol);
-		board.initPlayer(firstPlayer.cNumber, firstPlayer.nCurrentPointRow, firstPlayer.nCurrentPointCol);
-
-		board.printBoard();
-
-		// input row and col from user - second plyer
-		cout << "\nSecond player starting point\nenter row: ";
-		cin >> secondPlayerRow;
-		cout << "\nenter colomn: ";
-		cin >> secondPlayerCol;
-		cout << endl;
-
-		Player secondPlayer('2', secondPlayerRow, secondPlayerCol);
-		board.initPlayer(secondPlayer.cNumber, secondPlayer.nCurrentPointRow, secondPlayer.nCurrentPointCol);
-
-		board.printBoard();
-
-		// input row and col from user - winning mat
-		cout << "Winning mat's top-left corner point\nenter row: ";
-		cin >> winningMatRow;
-		cout << "\nenter colomn: ";
-		cin >> winningMatCol;
-		cout << endl;
-
-		// input dimentions from user - winning mat
-		cout << "\nWinning mat's dimentions\nenter height: ";
-		cin >> winningMatHeight;
-		cout << "\nenter width: ";
-		cin >> winningMatWidth;
-		cout << endl;
-
-		board.initWinningMat(winningMatRow, winningMatCol, winningMatHeight, winningMatWidth);
-
-#pragma endregion
-
-		board.printBoard();
-
-		while (!firstWinningFlag && !secondWinningFlag)
+		if (choice > 2 || choice < 1)
 		{
-			cout << "Please enter your step: \n"
-				"1 - Up\n"
-				"2 - Down\n"
-				"3 - Right\n"
-				"4 - Left\n"
-				<< endl;
-			cin >> playerMoveChoice;
+			cout << "Wrong choice." << endl;
+		}
+		else if (choice == 1)
+		{
+			cout << "You chose to play!\n" << endl;
 
-			if (firstTurnFlag && !secondTurnFlag && !firstWinningFlag && !secondWinningFlag)
-			{
-				firstPlayerRow = firstPlayer.nCurrentPointRow;
-				firstPlayerCol = firstPlayer.nCurrentPointCol;
+			cout << "The game board is empty, you need to fill it up" << endl;
+			board.printBoard();
 
-				movePlayer(playerMoveChoice, firstPlayer, board);
+			// input row and col from user - first player
+			cout << "First player starting point\nenter row: ";
+			cin >> firstPlayerRow;
+			cout << "\nenter colomn: ";
+			cin >> firstPlayerCol;
+			cout << endl;
 
-				if (board.placePlayer(firstPlayer.cNumber, firstPlayer.nCurrentPointRow, firstPlayer.nCurrentPointCol, firstPlayerRow, firstPlayerCol))
-				{
-					cout << "game over" << endl;
-					firstWinningFlag = true;
-				}
-			}
-			else if (secondTurnFlag && !firstTurnFlag && !firstWinningFlag && !secondWinningFlag)
-			{
-				secondPlayerRow = secondPlayer.nCurrentPointRow;
-				secondPlayerCol = secondPlayer.nCurrentPointCol;
-
-				movePlayer(playerMoveChoice, secondPlayer, board);
-
-				if (board.placePlayer(secondPlayer.cNumber, secondPlayer.nCurrentPointRow, secondPlayer.nCurrentPointCol, secondPlayerRow, secondPlayerCol))
-				{
-					cout << "game over" << endl;
-					secondWinningFlag = true;
-				}
-			}
+			Player firstPlayer('1', firstPlayerRow, firstPlayerCol);
+			board.initPlayer(firstPlayer.cNumber, firstPlayer.nCurrentPointRow, firstPlayer.nCurrentPointCol);
 
 			board.printBoard();
 
-			switchTurns(firstTurnFlag, secondTurnFlag);
+			// input row and col from user - second plyer
+			cout << "\nSecond player starting point\nenter row: ";
+			cin >> secondPlayerRow;
+			cout << "\nenter colomn: ";
+			cin >> secondPlayerCol;
+			cout << endl;
 
+			Player secondPlayer('2', secondPlayerRow, secondPlayerCol);
+			board.initPlayer(secondPlayer.cNumber, secondPlayer.nCurrentPointRow, secondPlayer.nCurrentPointCol);
+
+			board.printBoard();
+
+			// input row and col from user - winning mat
+			cout << "Winning mat's top-left corner point\nenter row: ";
+			cin >> winningMatRow;
+			cout << "\nenter colomn: ";
+			cin >> winningMatCol;
+			cout << endl;
+
+			// input dimentions from user - winning mat
+			cout << "\nWinning mat's dimentions\nenter height: ";
+			cin >> winningMatHeight;
+			cout << "\nenter width: ";
+			cin >> winningMatWidth;
+			cout << endl;
+
+			board.initWinningMat(winningMatRow, winningMatCol, winningMatHeight, winningMatWidth);
+
+#pragma endregion
+
+			board.printBoard();
+
+			while (!firstWinningFlag && !secondWinningFlag)
+			{
+				cout << "Please enter your step: \n"
+					"1 - Up\n"
+					"2 - Down\n"
+					"3 - Right\n"
+					"4 - Left\n"
+					<< endl;
+				cin >> playerMoveChoice;
+
+				if (firstTurnFlag && !secondTurnFlag && !firstWinningFlag && !secondWinningFlag)
+				{
+					firstPlayerRow = firstPlayer.nCurrentPointRow;
+					firstPlayerCol = firstPlayer.nCurrentPointCol;
+
+					movePlayer(playerMoveChoice, firstPlayer, board);
+
+					if (board.placePlayer(firstPlayer.cNumber, firstPlayer.nCurrentPointRow, firstPlayer.nCurrentPointCol, firstPlayerRow, firstPlayerCol))
+					{
+						cout << "game over" << endl;
+						firstWinningFlag = true;
+					}
+				}
+				else if (secondTurnFlag && !firstTurnFlag && !firstWinningFlag && !secondWinningFlag)
+				{
+					secondPlayerRow = secondPlayer.nCurrentPointRow;
+					secondPlayerCol = secondPlayer.nCurrentPointCol;
+
+					movePlayer(playerMoveChoice, secondPlayer, board);
+
+					if (board.placePlayer(secondPlayer.cNumber, secondPlayer.nCurrentPointRow, secondPlayer.nCurrentPointCol, secondPlayerRow, secondPlayerCol))
+					{
+						cout << "game over" << endl;
+						secondWinningFlag = true;
+					}
+				}
+
+				board.printBoard();
+
+				switchTurns(firstTurnFlag, secondTurnFlag);
+
+			}
 		}
 	}
-
-	else
-	{
-		cout << "You chose to exit the game..." << endl;
-	}
+	
+	cout << "You chose to exit the game..." << endl;
 
 }
 
